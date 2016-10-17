deb24cb4c199734f4eb3378407ed06cb6341fd5f
Diego Laurentino <diego.r.laurentino@gmail.com>
2016-10-16 23:44:30 -0200

    Bug fixes and enhanced paddle.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,6 +6,7 @@ public class Brick : MonoBehaviour {
 	public static int breakableCount = 0;
 	public AudioClip crack;
 	public Sprite[] hitSprites;
+	public GameObject smoke;
 
 	private int timesHit;
 	private LevelManager levelManager;
@@ -41,6 +42,7 @@ public class Brick : MonoBehaviour {
 		if (timesHit >= maxHits) {
 			breakableCount--;
 			levelManager.BrickDestroyed();
+			PuffSmoke();
 			Destroy (gameObject);
 		} 
 		else {
@@ -48,10 +50,19 @@ public class Brick : MonoBehaviour {
 		}
 	}
 
-	void LoadSprites () {
+	void PuffSmoke () { 
+		GameObject smokePuff = Instantiate(smoke, gameObject.transform.position, Quaternion.identity) as GameObject;
+		smokePuff.GetComponent<ParticleSystem>().startColor = gameObject.GetComponent<SpriteRenderer>().color;
+	}
+
+	void LoadSprites ()
+	{
 		int spriteIndex = timesHit - 1;
-		if(hitSprites[spriteIndex]) {
-			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		if (hitSprites [spriteIndex] != null) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		} 
+		else {
+			Debug.LogError("Brick sprite missing!");
 		}
 	}
 
